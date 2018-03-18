ce7599f01877c63a236c35ba397f5b00844ed237
shikha.gupta <shikha.gupta@quikr.com>
2018-03-18 22:30:35 +0530

    Linked list cycle start node

---
+++ src/array/RemoveElement.java
@@ -0,0 +1,7 @@
+package array;
+
+/**
+ * Created by Shikha on 16/03/18.
+ */
+public class RemoveElement {
+}
--- src/linkedlist/LinkedListCycleII.java
+++ src/linkedlist/LinkedListCycleII.java
@@ -1,7 +1,43 @@
 package linkedlist;
 
+import java.util.List;
+
 /**
  * Created by Shikha on 16/03/18.
+ * Given a linked list, return the node where the cycle begins.
+ * If there is no cycle, return null.
+ * Note: Do not modify the linked list.
  */
 public class LinkedListCycleII {
+    public static ListNode detectCycle(ListNode head) {
+        if(head==null)
+            return null;
+        ListNode fast =head;
+        ListNode slow =head;
+
+        while (fast!=null && fast.next!=null){
+            slow= slow.next;
+            fast= fast.next.next;
+            if(slow==fast){
+                ListNode temp = head;
+                while (temp != null) {
+                    if (temp == slow)
+                        return temp;
+                    temp = temp.next;
+                    slow = slow.next;
+                };
+            }
+
+        }
+
+        return null;
+
+    }
+
+    public static void main(String[] args){
+        ListNode head = ListNode.constructList();
+        System.out.print("\nOriginal List:");
+        ListNode.displayList(head);
+        detectCycle(head);
+    }
 }
