ef6557e3df92268728801c46ecc4670d4747a01d
shikha.gupta <shikha.gupta@quikr.com>
2018-03-23 20:23:02 +0530

    Remove Linked List Elements

--- src/linkedlist/RemoveLinkedListElements.java
+++ src/linkedlist/RemoveLinkedListElements.java
@@ -8,4 +8,30 @@ package linkedlist;
  * Return: 1 --> 2 --> 3 --> 4 --> 5
  */
 public class RemoveLinkedListElements {
+    public static ListNode removeElements(ListNode head, int val) {
+        if(head==null)
+            return head;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next=head;
+        ListNode temp = head;
+        ListNode prev = dummyHead;
+
+        while (temp!=null){
+            if(temp.val==val){
+                prev.next=temp.next;
+            }else{
+                prev=temp;
+            }
+            temp= temp.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args){
+        ListNode head = ListNode.constructList();
+        ListNode.displayList(head);
+        System.out.println();
+        head = removeElements(head,1);
+        ListNode.displayList(head);
+    }
 }
